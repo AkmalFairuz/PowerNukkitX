b7a919642e37803c05552058179390183c52f061
AkmalFairuz <akmalf475@gmail.com>
2024-11-10 16:00:11 +0700

    ProtocolInfo: fix typo

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -12,7 +12,7 @@ public interface ProtocolInfo {
      * Actual Minecraft: PE protocol version
      */
     int CURRENT_PROTOCOL = ProtocolInfo.PROTOCOL_748;
-    int PROTOCOL_748 = dynamic(48); // 1.21.40
+    int PROTOCOL_748 = dynamic(748); // 1.21.40
     int PROTOCOL_729 = dynamic(729); // 1.21.30
 
     int[] COMPATIBLE_PROTOCOLS = new int[]{
